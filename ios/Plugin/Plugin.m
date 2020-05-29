dcc7c43a07e7944ac29cbd44a5ad62c5c2c72949
Priyank Patel <priyank.patel@stackspace.ca>
2020-05-29 15:27:40 -0400

    Add iOS implementation

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -4,5 +4,10 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(TextToSpeech, "TextToSpeech",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(speak, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(stop, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setSpeechRate, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setPitchRate, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(openInstall, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getSupportedLanguages, CAPPluginReturnPromise);
 )
--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -1,4 +1,4 @@
-import Foundation
+import AVFoundation
 import Capacitor
 
 /**
@@ -6,12 +6,96 @@ import Capacitor
  * here: https://capacitor.ionicframework.com/docs/plugins/ios
  */
 @objc(TextToSpeech)
-public class TextToSpeech: CAPPlugin {
+public class TextToSpeech: CAPPlugin, AVSpeechSynthesizerDelegate {
     
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
+    var ttsSynthesizer: AVSpeechSynthesizer?
+    var hasInitialized: Bool = false
+    var supportedLangs = AVSpeechSynthesisVoice.speechVoices()
+    var ttsUtterance: AVSpeechUtterance?
+    
+    public func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        do {
+            try AVAudioSession.sharedInstance().setActive(false, options: [])
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.ambient)
+            try AVAudioSession.sharedInstance().setActive(true, options: [])
+        } catch {}
+    }
+    
+    public override func load() {
+        if (!self.hasInitialized) {
+            self.ttsSynthesizer = AVSpeechSynthesizer()
+            self.ttsSynthesizer?.delegate = self
+            self.hasInitialized = true
+        }
+    }
+    
+    @objc func speak(_ call: CAPPluginCall) {
+        let text = call.getString("text") ?? ""
+        let locale = call.getString("locale") ?? "en-US"
+        let speechRate = call.getDouble("speechRate") ?? 1.0
+        let pitchRate = call.getDouble("pitchRate") ?? 1.0
+        let category = call.getString("category") ?? "ambient"
+        
+        do {
+            try AVAudioSession.sharedInstance().setActive(false, options: [])
+        } catch {}
+        
+        if (category == "ambient") {
+            do {
+                try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.ambient)
+            }  catch {}
+        } else {
+            do {
+                try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+            } catch {}
+        }
+        
+        self.ttsSynthesizer?.stopSpeaking(at: .immediate)
+        
+        self.ttsUtterance = type(of: AVSpeechUtterance()).init(string: text)
+        self.ttsUtterance?.voice = AVSpeechSynthesisVoice(language: locale)
+        self.ttsUtterance?.rate = Float(speechRate)
+        self.ttsUtterance?.pitchMultiplier = Float(pitchRate)
+        
+        call.success()
+    }
+    
+    @objc func stop(_ call: CAPPluginCall) {
+        if self.ttsSynthesizer != nil {
+            self.ttsSynthesizer?.pauseSpeaking(at: .immediate)
+            self.ttsSynthesizer?.stopSpeaking(at: .immediate)
+            
+            call.success()
+        }
+    }
+    
+    @objc func openInstall(_ call: CAPPluginCall) {
+        call.success()
+    }
+    
+    @objc func setSpeechRate(_ call: CAPPluginCall) {
+        if call.hasOption("speechRate") {
+            let speechRate = call.getFloat("speechRate")
+            
+            if speechRate != nil {
+                self.ttsUtterance?.rate = speechRate!
+            }
+        }
+    }
+    
+    @objc func setPitchRate(_ call: CAPPluginCall) {
+        if call.hasOption("pitchRate") {
+            let pitchRate = call.getFloat("pitchRate")
+            
+            if pitchRate != nil {
+                self.ttsUtterance?.pitchMultiplier = pitchRate!
+            }
+        }
+    }
+    
+    @objc func getSupportedLanguages(_ call: CAPPluginCall) {
         call.success([
-            "value": value
+            "languages": supportedLangs
         ])
     }
 }
