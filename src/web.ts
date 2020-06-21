a8a74d35ab62ece0b37726cb06153d38403c8252
Priyank Patel <priyank.patel@stackspace.ca>
2020-06-21 17:26:09 -0400

    Update syntax

--- src/web.ts
+++ src/web.ts
@@ -1,31 +1,36 @@
-import { WebPlugin } from '@capacitor/core';
-import { TextToSpeechPlugin, TTSOptions } from './definitions';
+import { WebPlugin } from "@capacitor/core";
+import { TextToSpeechPlugin, TTSOptions } from "./definitions";
 
 export class TextToSpeechWeb extends WebPlugin implements TextToSpeechPlugin {
   constructor() {
     super({
-      name: 'TextToSpeech',
-      platforms: ['web']
+      name: "TextToSpeech",
+      platforms: ["web"],
     });
   }
   speak(options: TTSOptions): Promise<void> {
     console.warn(options);
     throw new Error("Method not implemented.");
   }
+
   stop(): Promise<void> {
     throw new Error("Method not implemented.");
   }
+
   getSupportedLanguages(): Promise<string> {
     throw new Error("Method not implemented.");
   }
+
   openInstall(): Promise<void> {
     throw new Error("Method not implemented.");
   }
-  setPitchRate(options: { pitchRate: number; }): Promise<void> {
+
+  setPitchRate(options: { pitchRate: number }): Promise<void> {
     console.warn(options);
     throw new Error("Method not implemented.");
   }
-  setSpeechRate(options: { speechRate: number; }): Promise<void> {
+
+  setSpeechRate(options: { speechRate: number }): Promise<void> {
     console.warn(options);
     throw new Error("Method not implemented.");
   }
@@ -35,5 +40,5 @@ const TextToSpeech = new TextToSpeechWeb();
 
 export { TextToSpeech };
 
-import { registerWebPlugin } from '@capacitor/core';
+import { registerWebPlugin } from "@capacitor/core";
 registerWebPlugin(TextToSpeech);
