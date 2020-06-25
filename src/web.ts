87facb3f4e9015bc70f00fe94206d4567ace3a0a
Priyank Patel <priyank.patel@stackspace.ca>
2020-06-25 09:16:55 -0400

    Fixes #1 - Add web support

--- src/web.ts
+++ src/web.ts
@@ -1,37 +1,102 @@
 import { WebPlugin } from "@capacitor/core";
-import { TextToSpeechPlugin, TTSOptions } from "./definitions";
+import {
+  TextToSpeechPlugin,
+  TTSOptions,
+  SpeechSynthesisVoice,
+} from "./definitions";
 
 export class TextToSpeechWeb extends WebPlugin implements TextToSpeechPlugin {
+  private speechSynthesizer: any;
+  private activeUtterance: any;
+  private notSupportedMessage =
+    "Speech Synthesizer is not yet initialized or supported.";
   constructor() {
     super({
       name: "TextToSpeech",
       platforms: ["web"],
     });
+
+    if (!this.speechSynthesizer && window && window.speechSynthesis) {
+      this.speechSynthesizer = window.speechSynthesis;
+    }
   }
   speak(options: TTSOptions): Promise<void> {
-    console.warn(options);
-    throw new Error("Method not implemented.");
+    return new Promise((resolve, reject) => {
+      if (!this.speechSynthesizer) {
+        reject(this.notSupportedMessage);
+        return;
+      }
+
+      if (!options) {
+        reject("No options were provided.");
+        return;
+      }
+
+      if (!options.text) {
+        reject("Text option was not provided");
+        return;
+      }
+
+      const { text, locale, rate, volume, voice, pitch } = options;
+
+      if (!this.activeUtterance) {
+        this.activeUtterance = new SpeechSynthesisUtterance();
+        this.activeUtterance.rate = rate >= 0.1 && rate <= 10 ? rate : 1;
+        this.activeUtterance.volume = volume >= 0 && volume <= 1 ? volume : 1;
+        this.activeUtterance.text = text;
+        this.activeUtterance.lang = locale;
+        this.activeUtterance.pitch = pitch >= 0 && pitch <= 2 ? pitch : 2;
+        if (voice) {
+          this.activeUtterance.voice = voice;
+        }
+        this.activeUtterance.onend = (ev: any) => {
+          resolve(ev);
+          this.activeUtterance = undefined;
+        };
+        this.activeUtterance.onerror = (ev: any) => {
+          reject(ev);
+          this.activeUtterance = undefined;
+        };
+
+        this.speechSynthesizer.speak(this.activeUtterance);
+      }
+    });
   }
 
   stop(): Promise<void> {
-    throw new Error("Method not implemented.");
+    return new Promise((resolve, reject) => {
+      if (!this.speechSynthesizer) {
+        reject(this.notSupportedMessage);
+        return;
+      }
+
+      this.speechSynthesizer.cancel();
+      resolve();
+    });
   }
 
-  getSupportedLanguages(): Promise<string> {
-    throw new Error("Method not implemented.");
+  getSupportedLanguages(): Promise<string | SpeechSynthesisVoice[]> {
+    return new Promise((resolve, reject) => {
+      if (!this.speechSynthesizer) {
+        reject(this.notSupportedMessage);
+        return;
+      }
+
+      resolve(this.speechSynthesizer.getVoices() as SpeechSynthesisVoice[]);
+    });
   }
 
   openInstall(): Promise<void> {
     throw new Error("Method not implemented.");
   }
 
-  setPitchRate(options: { pitchRate: number }): Promise<void> {
-    console.warn(options);
+  setPitchRate(_options: { pitchRate: number }): Promise<void> {
+    // Pitch rate cannot be set while engine is active
     throw new Error("Method not implemented.");
   }
 
-  setSpeechRate(options: { speechRate: number }): Promise<void> {
-    console.warn(options);
+  setSpeechRate(_options: { speechRate: number }): Promise<void> {
+    // Speech rate cannot be set while engine is active
     throw new Error("Method not implemented.");
   }
 }
