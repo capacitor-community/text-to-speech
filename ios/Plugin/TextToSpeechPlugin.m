e0a9729b49fe70babf6c0e5e472b1d8613486873
Robin Genz <mail@robingenz.dev>
2021-03-13 23:24:30 +0100

    ios: fix

--- ios/Plugin/TextToSpeechPlugin.m
+++ ios/Plugin/TextToSpeechPlugin.m
@@ -4,5 +4,11 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(TextToSpeechPlugin, "TextToSpeech",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(speak, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(stop, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setSpeechRate, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setPitchRate, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(openInstall, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getSupportedLanguages, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getSupportedVoices, CAPPluginReturnPromise);
 )
