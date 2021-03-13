5c5234dd371dd011858e8f1944e22940a14a3b5e
Robin Genz <mail@robingenz.dev>
2021-03-13 14:41:19 +0100

    wip: web

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,48 @@
 export interface TextToSpeechPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+  speak(options: TTSOptions): Promise<void>;
+  stop(): Promise<void>;
+  getSupportedLanguages(): Promise<{ languages: string[] }>;
+  getSupportedVoices(): Promise<{ voices: SpeechSynthesisVoice[] }>;
+  openInstall(): Promise<void>;
+  setPitchRate(options: { pitchRate: number }): Promise<void>;
+  setSpeechRate(options: { speechRate: number }): Promise<void>;
+}
+
+export interface TTSOptions {
+  text: string;
+  locale?: string;
+  speechRate?: number;
+  pitchRate?: number;
+  volume?: number;
+  voice?: number; // Web only
+  category?: string; // iOS only
+}
+
+/**
+ * The SpeechSynthesisVoice interface represents a voice that the system supports.
+ */
+export interface SpeechSynthesisVoice {
+  /**
+   * Specifies whether the voice is the default voice for the current app (`true`) or not (`false`).
+   */
+  default: boolean;
+  /**
+   * BCP 47 language tag indicating the language of the voice.
+   * Example: `en-US`
+   */
+  lang: string;
+  /**
+   * Specifies whether the voice is supplied by a local (`true`) or remote (`false`) speech synthesizer service.
+   */
+  localService: boolean;
+  /**
+   * Human-readable name that represents the voice.
+   * Example: `Microsoft Zira Desktop - English (United States)`
+   */
+  name: string;
+  /**
+   * Type of URI and location of the speech synthesis service for this voice.
+   * Example: `urn:moz-tts:sapi:Microsoft Zira Desktop - English (United States)?en-US`
+   */
+  voiceURI: string;
 }
--- src/web.ts
+++ src/web.ts
@@ -1,10 +1,110 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { TextToSpeechPlugin } from './definitions';
+import type { TextToSpeechPlugin, TTSOptions } from './definitions';
 
 export class TextToSpeechWeb extends WebPlugin implements TextToSpeechPlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
+  private speechSynthesis: SpeechSynthesis | null = null;
+
+  constructor() {
+    super({
+      name: 'TextToSpeech',
+      platforms: ['web'],
+    });
+    if ('speechSynthesis' in window) {
+      this.speechSynthesis = window.speechSynthesis;
+    }
+  }
+
+  public async speak(options: TTSOptions): Promise<void> {
+    if (!this.speechSynthesis) {
+      this.throwUnsupportedError();
+    }
+    await this.stop();
+    const speechSynthesis = this.speechSynthesis;
+    const utterance = this.createSpeechSynthesisUtterance(options);
+    return new Promise((resolve, reject) => {
+      utterance.onend = () => {
+        resolve();
+      };
+      utterance.onerror = (event: any) => {
+        reject(event);
+      };
+      speechSynthesis.speak(utterance);
+    });
+  }
+
+  public async stop(): Promise<void> {
+    if (!this.speechSynthesis) {
+      this.throwUnsupportedError();
+    }
+    this.speechSynthesis.cancel();
+  }
+
+  public async getSupportedLanguages(): Promise<{ languages: string[] }> {
+    const voices = this.getSpeechSynthesisVoices();
+    const languages = voices.map(voice => voice.lang);
+    const filteredLanguages = languages.filter((v, i, a) => a.indexOf(v) == i);
+    return { languages: filteredLanguages };
+  }
+
+  public async getSupportedVoices(): Promise<{
+    voices: SpeechSynthesisVoice[];
+  }> {
+    const voices = this.getSpeechSynthesisVoices();
+    return { voices };
+  }
+
+  public async openInstall(): Promise<void> {
+    this.throwUnimplementedError();
+  }
+
+  public async setPitchRate(_options: { pitchRate: number }): Promise<void> {
+    this.throwUnimplementedError();
+  }
+
+  public async setSpeechRate(_options: { speechRate: number }): Promise<void> {
+    this.throwUnimplementedError();
+  }
+
+  private createSpeechSynthesisUtterance(
+    options: TTSOptions,
+  ): SpeechSynthesisUtterance {
+    const voices = this.getSpeechSynthesisVoices();
+    const utterance = new SpeechSynthesisUtterance();
+    const { text, locale, speechRate, volume, voice, pitchRate } = options;
+    if (voice) {
+      utterance.voice = voices[voice];
+    }
+    if (volume) {
+      utterance.volume = volume >= 0 && volume <= 1 ? volume : 1;
+    }
+    if (speechRate) {
+      utterance.rate = speechRate >= 0.1 && speechRate <= 10 ? speechRate : 1;
+    }
+    if (pitchRate) {
+      utterance.pitch = pitchRate >= 0 && pitchRate <= 2 ? pitchRate : 2;
+    }
+    if (locale) {
+      utterance.lang = locale;
+    }
+    utterance.text = text;
+    return utterance;
+  }
+
+  private getSpeechSynthesisVoices(): SpeechSynthesisVoice[] {
+    if (!this.speechSynthesis) {
+      this.throwUnsupportedError();
+    }
+    return this.speechSynthesis.getVoices();
+  }
+
+  private throwUnsupportedError(): never {
+    throw this.unavailable(
+      'SpeechSynthesis API not available in this browser.',
+    );
+  }
+
+  private throwUnimplementedError(): never {
+    throw this.unimplemented('Not implemented on web.');
   }
 }
